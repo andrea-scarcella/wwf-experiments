62851214f6e912a9ebffa8cd6e6b9c541add5419
Andrea Scarcella <andrea.scarcella.as@gmail.com>
2013-10-24 16:10:04 +0200

    Long-running workflow sample

--- WF45GettingStartedTutorial/NumberGuessWorkflowHost/Program.cs
+++ WF45GettingStartedTutorial/NumberGuessWorkflowHost/Program.cs
@@ -6,6 +6,7 @@ using System.Activities.Statements;
 using NumberGuessWorkflowActivities;
 using System.Threading;
 using System.Collections.Generic;
+using System.Windows.Forms;
 
 namespace NumberGuessWorkflowHost
 {
@@ -14,62 +15,67 @@ namespace NumberGuessWorkflowHost
 	{
 		static void Main(string[] args)
 		{
-			AutoResetEvent syncEvent = new AutoResetEvent(false);
-			AutoResetEvent idleEvent = new AutoResetEvent(false);
-
-			var inputs = new Dictionary<string, object>() { { "MaxNumber", 100 } };
-
-			WorkflowApplication wfApp =
-				//new WorkflowApplication(new FlowchartNumberGuessWorkflow.FlowchartNumberGuessWorkflow(), inputs);
-				new WorkflowApplication(new StateMachineNumberGuessWorkflow(), inputs);
-
-			wfApp.Completed = delegate(WorkflowApplicationCompletedEventArgs e)
-			{
-				int Turns = Convert.ToInt32(e.Outputs["Turns"]);
-				Console.WriteLine("Congratulations, you guessed the number in {0} turns.", Turns);
-
-				syncEvent.Set();
-			};
-
-			wfApp.Aborted = delegate(WorkflowApplicationAbortedEventArgs e)
-			{
-				Console.WriteLine(e.Reason);
-				syncEvent.Set();
-			};
-
-			wfApp.OnUnhandledException = delegate(WorkflowApplicationUnhandledExceptionEventArgs e)
-			{
-				Console.WriteLine(e.UnhandledException.ToString());
-				return UnhandledExceptionAction.Terminate;
-			};
-			wfApp.Idle = delegate(WorkflowApplicationIdleEventArgs e)
-			{
-				idleEvent.Set();
-			};
-
-			wfApp.Run();
-
-			// Loop until the workflow completes.
-			WaitHandle[] handles = new WaitHandle[] { syncEvent, idleEvent };
-			while (WaitHandle.WaitAny(handles) != 0)
-			{
-				// Gather the user input and resume the bookmark.
-				bool validEntry = false;
-				while (!validEntry)
-				{
-					int Guess;
-					if (!Int32.TryParse(Console.ReadLine(), out Guess))
-					{
-						Console.WriteLine("Please enter an integer.");
-					}
-					else
-					{
-						validEntry = true;
-						wfApp.ResumeBookmark("EnterGuess", Guess);
-					}
-				}
-			}
-
+			Application.EnableVisualStyles();
+			Application.Run(new WorkflowHostForm());
 		}
+		//static void Main(string[] args)
+		//{
+		//	AutoResetEvent syncEvent = new AutoResetEvent(false);
+		//	AutoResetEvent idleEvent = new AutoResetEvent(false);
+
+		//	var inputs = new Dictionary<string, object>() { { "MaxNumber", 100 } };
+
+		//	WorkflowApplication wfApp =
+		//		//new WorkflowApplication(new FlowchartNumberGuessWorkflow.FlowchartNumberGuessWorkflow(), inputs);
+		//		new WorkflowApplication(new StateMachineNumberGuessWorkflow(), inputs);
+
+		//	wfApp.Completed = delegate(WorkflowApplicationCompletedEventArgs e)
+		//	{
+		//		int Turns = Convert.ToInt32(e.Outputs["Turns"]);
+		//		Console.WriteLine("Congratulations, you guessed the number in {0} turns.", Turns);
+
+		//		syncEvent.Set();
+		//	};
+
+		//	wfApp.Aborted = delegate(WorkflowApplicationAbortedEventArgs e)
+		//	{
+		//		Console.WriteLine(e.Reason);
+		//		syncEvent.Set();
+		//	};
+
+		//	wfApp.OnUnhandledException = delegate(WorkflowApplicationUnhandledExceptionEventArgs e)
+		//	{
+		//		Console.WriteLine(e.UnhandledException.ToString());
+		//		return UnhandledExceptionAction.Terminate;
+		//	};
+		//	wfApp.Idle = delegate(WorkflowApplicationIdleEventArgs e)
+		//	{
+		//		idleEvent.Set();
+		//	};
+
+		//	wfApp.Run();
+
+		//	// Loop until the workflow completes.
+		//	WaitHandle[] handles = new WaitHandle[] { syncEvent, idleEvent };
+		//	while (WaitHandle.WaitAny(handles) != 0)
+		//	{
+		//		// Gather the user input and resume the bookmark.
+		//		bool validEntry = false;
+		//		while (!validEntry)
+		//		{
+		//			int Guess;
+		//			if (!Int32.TryParse(Console.ReadLine(), out Guess))
+		//			{
+		//				Console.WriteLine("Please enter an integer.");
+		//			}
+		//			else
+		//			{
+		//				validEntry = true;
+		//				wfApp.ResumeBookmark("EnterGuess", Guess);
+		//			}
+		//		}
+		//	}
+
+		//}
 	}
 }
