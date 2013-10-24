d89cf7568edc499c75fd54308b4bb75d2ea84012
Andrea Scarcella <andrea.scarcella.as@gmail.com>
2013-10-24 17:32:44 +0200

    Added side-by-side hosting capabilities

--- WF45GettingStartedTutorial/NumberGuessWorkflowHost/WorkflowVersionMap.cs
+++ WF45GettingStartedTutorial/NumberGuessWorkflowHost/WorkflowVersionMap.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using NumberGuessWorkflowActivities;
+using System.Reflection;
+using System.IO;
 
 namespace NumberGuessWorkflowHost
 {
@@ -17,6 +19,11 @@ namespace NumberGuessWorkflowHost
 		static public WorkflowIdentity FlowchartNumberGuessIdentity;
 		static public WorkflowIdentity SequentialNumberGuessIdentity;
 
+		// v1 identities.
+		static public WorkflowIdentity StateMachineNumberGuessIdentity_v1;
+		static public WorkflowIdentity FlowchartNumberGuessIdentity_v1;
+		static public WorkflowIdentity SequentialNumberGuessIdentity_v1;
+
 		static WorkflowVersionMap()
 		{
 			map = new Dictionary<WorkflowIdentity, Activity>();
@@ -25,24 +32,61 @@ namespace NumberGuessWorkflowHost
 			StateMachineNumberGuessIdentity = new WorkflowIdentity
 			{
 				Name = "StateMachineNumberGuessWorkflow",
-				Version = new Version(1, 0, 0, 0)
+				// Version = new Version(1, 0, 0, 0),
+				Version = new Version(2, 0, 0, 0)
 			};
 
 			FlowchartNumberGuessIdentity = new WorkflowIdentity
 			{
 				Name = "FlowchartNumberGuessWorkflow",
-				Version = new Version(1, 0, 0, 0)
+				// Version = new Version(1, 0, 0, 0),
+				Version = new Version(2, 0, 0, 0)
 			};
 
 			SequentialNumberGuessIdentity = new WorkflowIdentity
 			{
 				Name = "SequentialNumberGuessWorkflow",
-				Version = new Version(1, 0, 0, 0)
+				// Version = new Version(1, 0, 0, 0),
+				Version = new Version(2, 0, 0, 0)
 			};
-
 			map.Add(StateMachineNumberGuessIdentity, new StateMachineNumberGuessWorkflow());
 			map.Add(FlowchartNumberGuessIdentity, new FlowchartNumberGuessWorkflow());
 			map.Add(SequentialNumberGuessIdentity, new SequentialNumberGuessWorkflow());
+			// Initialize the previous workflow version identities.
+			StateMachineNumberGuessIdentity_v1 = new WorkflowIdentity
+			{
+				Name = "StateMachineNumberGuessWorkflow",
+				Version = new Version(1, 0, 0, 0)
+			};
+
+			FlowchartNumberGuessIdentity_v1 = new WorkflowIdentity
+			{
+				Name = "FlowchartNumberGuessWorkflow",
+				Version = new Version(1, 0, 0, 0)
+			};
+
+			SequentialNumberGuessIdentity_v1 = new WorkflowIdentity
+			{
+				Name = "SequentialNumberGuessWorkflow",
+				Version = new Version(1, 0, 0, 0)
+			};
+
+			// Add the previous version workflow identities to the dictionary along with
+			// the corresponding workflow definitions loaded from the v1 assembly.
+			// Assembly.LoadFile requires an absolute path so convert this relative path
+			// to an absolute path.
+			string v1AssemblyPath = @"..\..\..\PreviousVersions\NumberGuessWorkflowActivities_v1.dll";
+			v1AssemblyPath = Path.GetFullPath(v1AssemblyPath);
+			Assembly v1Assembly = Assembly.LoadFile(v1AssemblyPath);
+
+			map.Add(StateMachineNumberGuessIdentity_v1,
+				v1Assembly.CreateInstance("NumberGuessWorkflowActivities.StateMachineNumberGuessWorkflow") as Activity);
+
+			map.Add(SequentialNumberGuessIdentity_v1,
+				v1Assembly.CreateInstance("NumberGuessWorkflowActivities.SequentialNumberGuessWorkflow") as Activity);
+
+			map.Add(FlowchartNumberGuessIdentity_v1,
+				v1Assembly.CreateInstance("NumberGuessWorkflowActivities.FlowchartNumberGuessWorkflow") as Activity);
 		}
 
 		public static Activity GetWorkflowDefinition(WorkflowIdentity identity)
