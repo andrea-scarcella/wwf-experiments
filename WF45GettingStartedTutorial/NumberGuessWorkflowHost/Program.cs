6fd20d56c1259ded9df72ce1756e21368eccf58f
Andrea Scarcella <andrea.scarcella.as@gmail.com>
2013-10-24 11:38:43 +0200

    Fixed flowchart number workflow namespace

--- WF45GettingStartedTutorial/NumberGuessWorkflowHost/Program.cs
+++ WF45GettingStartedTutorial/NumberGuessWorkflowHost/Program.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Activities;
 using System.Activities.Statements;
 using NumberGuessWorkflowActivities;
-using FlowchartNumberGuessWorkflow;
 using System.Threading;
 using System.Collections.Generic;
 
